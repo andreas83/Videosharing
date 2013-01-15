9130677d0f1a93435df2c481370bf6a8b6d0eb49
nfo <nfo@f9ade1f2-beff-4255-988d-befa2c79c1bc>
2013-01-15 12:52:17 +0000

    #61 added ffmpeg class and sample upload

--- app/views/Video/Manager/upload.php
+++ app/views/Video/Manager/upload.php
@@ -1,4 +1,21 @@
-        <form>
-                <div id="queue"></div>
-                <input id="file_upload" name="file_upload" type="file" multiple="true">
-        </form>
+    <form method="POST" src="<?php echo Config::get('address'); ?>/Video/Manager/Upload" enctype="multipart/form-data">
+            <div id="queue"></div>
+            <input id="file_upload" name="file_upload" type="file" multiple="true">
+            <input type="submit" name="upload">
+    </form>
+
+<?php
+if($view->thumb):
+    echo '<img src="'.$view->thumb.'_thumbs1/00000001.png" width="300">';
+    echo '<img src="'.$view->thumb.'_thumbs2/00000001.png" width="300">';
+    echo '<img src="'.$view->thumb.'_thumbs3/00000001.png" width="300">';
+
+?>
+ <video width="320" height="240" controls>
+
+  <source src="<?=$view->thumb;?>.webm" type="video/webm">
+  <source src="<?=$view->thumb;?>.mp4" type="video/mp4">
+</video> 
+<?
+endif;
+?>
