ce32fd94f2d1a82d3a946f9021f02f440d60b817
Andreas Beder <office@moving-bytes.at>
2013-01-16 11:08:20 +0000

    cleanup init

--- init.php
+++ init.php
@@ -2,31 +2,6 @@
 // ATTENTION !!!!
 // please include only files if you know how the
 // autoloader works, usealy there is no need for that
-DEFINE('INCLUDE_PATTERN', 'videostream');
-
-/**
- * only needed for cli methods
- * to find it's own relative path to the application
- *
- * @param $path string            
- *
- *
- */
-if ( !function_exists('self_aware') )
-{
-
-
-    function self_aware()
-    {
-
-        if ( ($cut = strpos(__DIR__, INCLUDE_PATTERN)) !== false )
-        {
-            return substr(__DIR__, 0, $cut);
-        }
-    }
-}
-
-$base = (string) (!empty($_SERVER['DOCUMENT_ROOT'])) ? $_SERVER['DOCUMENT_ROOT'] : self_aware();
 
 include_once ("lib/config.php");
 Config::create_instance();
