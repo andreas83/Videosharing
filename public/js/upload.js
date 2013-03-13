246b8f7c4fb15f41cfee320ef7ac9a3d6ea9bf8d
Andreas Beder <office@moving-bytes.at>
2013-03-13 12:14:04 +0000

    added upload progressbar

--- public/js/upload.js
+++ public/js/upload.js
@@ -1,5 +1,28 @@
 $(function() {
-    
+    if($('#upload_form').length!=0)
+    {
+    $('#upload_form').fileupload({
+        dataType: 'html',
+        add: function (e, data) {
+            data.context = $('<button class="btn btn-inverse"/>').text('Upload')
+                .appendTo("#upload_form")
+                .click(function () {
+                    $(this).replaceWith($('<p/>').text('Uploading...'));
+                    data.submit();
+                });
+        },
+        done: function (e, data) {
+		jQuery("html").html(data.result);
+        },
+        progressall: function (e, data) {
+        var progress = parseInt(data.loaded / data.total * 100, 10);
+        $('.bar').css(
+            'width',
+            progress + '%'
+        );
+        }
+    });
+    }
     var selectedThumb=$("#thumb").val();
     var i=0;
     $(".thumbnail").each(function(){
