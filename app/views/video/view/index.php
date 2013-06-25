7ede357c70ef47edcea3674204473fcc81224a87
nfo <nfo@f9ade1f2-beff-4255-988d-befa2c79c1bc>
2013-06-25 21:52:55 +0000

    some code cleanup

--- app/views/video/view/index.php
+++ app/views/video/view/index.php
@@ -8,22 +8,12 @@ if(isset($view->error) && !empty($view->error))
     if($view->showRegister)
     {
         echo'<div class="alert alert-info">
-                <h2>'.$view->error.'</h2>';
-        echo "</div>";
-        echo '<div class="row">
-                <div class="span1">
-                  <form method="get" action="'. Config::get('address').'/user/register">
-                    <input class="btn btn-large btn-primary" type="submit"  value="'. _("Register").' ">
-                  </form></div>
-                <div class="span1">
+                <h2>'.$view->error.'</h2>
                     <form method="get" action="'. Config::get('address').'/user/login">
-                    <input class="btn btn-large btn-success" type="submit"  value="'. _("Login").' ">
-                  </form>
-                </div>
-               </div>
-              ';
-        
-        
+                    <input class="btn btn-success" type="submit"  value="'. _("Sign Up").' ">
+                  </form>';
+        echo "</div>";
+
     }else
     {
         echo'<div class="alert alert-error">
@@ -35,17 +25,18 @@ if(isset($view->error) && !empty($view->error))
 }
 ?>
 
-<div class="row">
+<div class="row-fluid">
 <video video="<?= $view->id; ?>" width="640" height="380" controls="controls"  poster="<?php echo Config::get('address'); ?>/public/video/<?php echo $view->user_id; ?>/<?= $view->id; ?>/thumb<?php echo $view->thumb; ?>.png">
             <source src="<?php echo Config::get('address'); ?>/public/video/<?php echo $view->user_id; ?>/<?= $view->id; ?>/<?= $view->id; ?>.mp4" type="video/mp4">          
             <source src="<?php echo Config::get('address'); ?>/public/video/<?php echo $view->user_id; ?>/<?= $view->id; ?>/<?= $view->id; ?>.webm" type="video/webm">
+
             <div class="mediaplayback" id="<?= $view->id; ?>" uid="<?php echo $view->user_id; ?>" video="<?= $view->id; ?>" thumb="<?= $view->thumb; ?>" style="width:640; height:480"></div>
      
 </video> 
 
 
 </div>
-<div class="row">
+<div class="row-fluid">
     <h1><?php echo $view->title; ?>
     <? if ($view->user_id == $_SESSION['user_id']):?>
     <form method="get" action="<?php echo Config::get('address'); ?>/video/manager/edit">
@@ -55,7 +46,7 @@ if(isset($view->error) && !empty($view->error))
     <? endif; ?>
 </h1> 
 </div>
-<div class="row">
+<div class="row-fluid">
 <?
 echo $view->desc;
 ?>
--- template/default/main.php
+++ template/default/main.php
@@ -26,3 +26,32 @@ foreach($data as $row)
 echo "</div>";
 ?>
 
+
+<div class="row-fluid"><h1>Only for Registered</h1></div>
+<div class="row-fluid">
+<?php
+$video = new Video();
+$data=$video->get_list(array("isConverted" => 1, "visibility_setting" => 2), 9);
+$i=0;
+foreach($data as $row) 
+{
+  if(($i%3)==0)
+  {
+      if($i!=1)
+          echo "</div>\n";
+      echo '<div class="row-fluid">';
+  }
+?>
+
+            <div class="span4">
+              <a href="/video/view?id=<?=$row->id; ?>"><img src="/video/view/thumbnail?id=<?=$row->id; ?>&amp;width=200&amp;height=100" width="200" height="100" class="img-polaroid" alt="thumbnail of <?=$row->title; ?>" /></a>
+              <h3><a href="/video/view?id=<?=$row->id; ?>"><?=$row->title; ?></a></h3>
+            </div>
+   
+<?
+  $i++;
+
+}
+echo "</div>";
+?>
+
