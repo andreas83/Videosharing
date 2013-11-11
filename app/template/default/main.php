7a7acd6de17b94419a032b0ecc6e35af7120ef83
Andreas Beder <office@moving-bytes.at>
2013-11-11 15:51:17 +0000

    #131

--- app/template/default/main.php
+++ app/template/default/main.php
@@ -29,28 +29,31 @@
     <div class="large-12 columns">
         <h1>Recent Videos</h1>
     </div>
+    <div class="row">'
     <?php
     $video = new Video();
     $data = $video->get_list(array("isConverted" => 1, "visibility_setting" => 1), "1, 9");
     $i = 0;
     foreach ($data as $row) {
-        if (($i % 3) == 0) {
+/*        if (($i % 3) == 0) {
             if ($i != 1)
                 echo "</div>\n";
             echo '<div class="row">';
         }
-        ?>
+*/  
+      ?>
 
         <div class="large-4 small-6 columns text-center">
             <a href="<?php echo Config::get('address'); ?>/video/view?id=<?= $row->id; ?>">
                 <img src="<?php echo Config::get('address'); ?>/video/view/thumbnail?id=<?= $row->id; ?>&amp;width=200&amp;height=100"  class="img-polaroid" alt="thumbnail of <?= $row->title; ?>" />
             </a>
-            <h4 class="hide-on-small"><a href="<?php echo Config::get('address'); ?>/video/view?id=<?= $row->id; ?>"><?= $row->title; ?></a></h4>
+            <h6 class="hide-for-small"><a href="<?php echo Config::get('address'); ?>/video/view?id=<?= $row->id; ?>"><?= $row->title; ?></a></h6>
         </div>
 
     <?
     $i++;
 }
+echo "</div>";
 echo "</div>";
 ?>
 
