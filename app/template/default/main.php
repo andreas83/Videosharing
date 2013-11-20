1c76262a6d3ad8c2c3c09533e246e639d833f9fe
Andreas Beder <office@moving-bytes.at>
2013-11-20 13:19:20 +0000

    #134

--- app/template/default/main.php
+++ app/template/default/main.php
@@ -8,12 +8,12 @@
     $i = 0;
     foreach ($data as $view) {
          ?> 
-            <video class="height-auto" video="<?= $view->id; ?>"  controls="controls" poster="<?php echo Config::get('address'); ?>/public/video/<?php echo $view->user_id; ?>/<?= $view->id; ?>/thumb<?php echo $view->thumb; ?>.png">
-            <source src="<?php echo Config::get('address'); ?>/public/video/<?php echo $view->user_id; ?>/<?= $view->id; ?>/<?= $view->id; ?>.mp4" type="video/mp4">          
-            <source src="<?php echo Config::get('address'); ?>/public/video/<?php echo $view->user_id; ?>/<?= $view->id; ?>/<?= $view->id; ?>.webm" type="video/webm">
+            <video class="height-auto" video="<?php echo $view->id; ?>"  controls="controls" poster="<?php echo Config::get('address'); ?>/public/video/<?php echo $view->user_id; ?>/<?php echo $view->id; ?>/thumb<?php echo $view->thumb; ?>.png">
+            <source src="<?php echo Config::get('address'); ?>/public/video/<?php echo $view->user_id; ?>/<?php echo $view->id; ?>/<?php echo $view->id; ?>.mp4" type="video/mp4">          
+            <source src="<?php echo Config::get('address'); ?>/public/video/<?php echo $view->user_id; ?>/<?php echo $view->id; ?>/<?php $view->id; ?>.webm" type="video/webm">
 
         </video> 
-	<?
+	<?php
     }
     ?>
         </div>
@@ -31,13 +31,13 @@
     ?>
 
         <div class="large-4 small-6 columns">
-            <a href="<?php echo Config::get('address'); ?>/video/view?id=<?= $row->id; ?>">
-                <img src="<?php echo Config::get('address'); ?>/video/view/thumbnail?id=<?= $row->id; ?>&amp;width=200&amp;height=100"  class="img-polaroid" alt="thumbnail of <?= $row->title; ?>" />
+            <a href="<?php echo Config::get('address'); ?>/video/view?id=<?php echo $row->id; ?>">
+                <img src="<?php echo Config::get('address'); ?>/video/view/thumbnail?id=<?php echo $row->id; ?>&amp;width=200&amp;height=100"  class="img-polaroid" alt="thumbnail of <?php echo $row->title; ?>" />
             </a>
-            <h6><a href="<?php echo Config::get('address'); ?>/video/view?id=<?= $row->id; ?>"><?= $row->title; ?></a></h6>
+            <h6><a href="<?php echo Config::get('address'); ?>/video/view?id=<?php echo $row->id; ?>"><?php echo $row->title; ?></a></h6>
         </div>
 
-    <?
+    <?php
     $i++;
 }
 echo "</div>";
