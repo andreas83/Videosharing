a2b8011b51bf975912a4e88dd810a688c67e3244
nfo <nfo@f9ade1f2-beff-4255-988d-befa2c79c1bc>
2013-11-21 13:35:14 +0000

    just source code foirmat

--- app/modul/user/view/video/index.php
+++ app/modul/user/view/video/index.php
@@ -1,7 +1,6 @@
 <?php
 if (isset($view->error) && !empty($view->error)) {
 
-
     echo '<div class="row">';
 
     echo'<div  class="alert-box radius">
@@ -27,7 +26,7 @@ if (count($view->inProgress) > 0):
     <div class="row">
         <div class="large-12 columns">
             <div data-alert class="alert-box success">
-    <?php echo _("Some videos are converted in the background"); ?>
+                <?php echo _("Some videos are converted in the background"); ?>
 
             </div>
         </div>
@@ -38,13 +37,13 @@ if (count($view->inProgress) > 0):
 foreach ($view->obj as $key) {
     ?>
     <div class="row">
-    
+
 
         <div class="large-6 columns">
-            <a href="<?php echo Config::get('address'); ?>/video/view?id=<?php echo  $key->id; ?>"><img class="img-thumbnail" src="<?php echo Config::get('address'); ?>/video/view/thumbnail?id=<?php echo  $key->id; ?>&width=380&height=200"></a>
+            <a href="<?php echo Config::get('address'); ?>/video/view?id=<?php echo $key->id; ?>"><img class="img-thumbnail" src="<?php echo Config::get('address'); ?>/video/view/thumbnail?id=<?php echo $key->id; ?>&width=380&height=200"></a>
         </div>
         <div class="large-6 columns">
-            <h2><a href="<?php echo Config::get('address'); ?>/video/view?id=<?php echo  $key->id; ?>"><?php echo $key->title; ?></a></h2> 
+            <h2><a href="<?php echo Config::get('address'); ?>/video/view?id=<?php echo $key->id; ?>"><?php echo $key->title; ?></a></h2> 
             <p><?php echo $key->descripton; ?></p>
         </div>
 
@@ -53,48 +52,48 @@ foreach ($view->obj as $key) {
 }
 if (count($view->obj) == 0 && count($view->inProgress) == 0):
     ?>
-<div class="row">
-    <div class="alert alert-info">
-        <h2><?php echo _("Ohoh, you did not Upload any Video so far"); ?></h2>
-        <button class="button" onclick="location.href='<?php echo Config::get('address'); ?>/video/manager/upload'" ><?php echo _("Upload Now"); ?></button>
+    <div class="row">
+        <div class="alert alert-info">
+            <h2><?php echo _("Ohoh, you did not Upload any Video so far"); ?></h2>
+            <button class="button" onclick="location.href='<?php echo Config::get('address'); ?>/video/manager/upload'" ><?php echo _("Upload Now"); ?></button>
+        </div>
     </div>
-</div>
 <?php endif;
 ?>
 
 <div class="row">
-    
 
-<?php
-if (count($view->obj) > 0):
-    ?>
-    <div class="large-12 columns">
-        <ul class="pagination">
+
     <?php
-    if ($view->page == 1) {
-        $url = "";
-        $class = "unavailable";
-    } else {
-        $res = $view->page - 1;
-        $url = Config::get('address') . "/user/video?page=" . $res;
-    }
-    ?>
-            <li><h3><a class="<?php echo $class; ?>" href="<?php echo $url; ?>">&laquo;</a></h3></li>
-            <?php
-            foreach (range(1, $view->Seiten) as $number) {
-                echo "<li><h3><a href=\"" . Config::get('address') . "/user/video?page=$number\">$number</a></h3></li>";
-            }
+    if (count($view->obj) > 0):
+        ?>
+        <div class="large-12 columns">
+            <ul class="pagination">
+                <?php
+                if ($view->page == 1) {
+                    $url = "";
+                    $class = "unavailable";
+                } else {
+                    $res = $view->page - 1;
+                    $url = Config::get('address') . "/user/video?page=" . $res;
+                }
+                ?>
+                <li><h3><a class="<?php echo $class; ?>" href="<?php echo $url; ?>">&laquo;</a></h3></li>
+                <?php
+                foreach (range(1, $view->Seiten) as $number) {
+                    echo "<li><h3><a href=\"" . Config::get('address') . "/user/video?page=$number\">$number</a></h3></li>";
+                }
 
-            if ($view->Seiten == $view->page) {
-                $url = "";
-                $class = "unavailable";
-            } else {
-                $res = $view->page + 1;
-                $url = "?page=" . $res;
-            }
-            ?>
-            <li><h3><a class="<?php echo $class; ?>" href="<?php echo $url; ?>">&raquo;</a></h3></li>
-        </ul>
+                if ($view->Seiten == $view->page) {
+                    $url = "";
+                    $class = "unavailable";
+                } else {
+                    $res = $view->page + 1;
+                    $url = "?page=" . $res;
+                }
+                ?>
+                <li><h3><a class="<?php echo $class; ?>" href="<?php echo $url; ?>">&raquo;</a></h3></li>
+            </ul>
 
             <?php
         endif;
